b685ea04b1707e8b0f424ece1bf16d712e954fce
xhrusvin <rusvin@xpresshealth.com>
2026-06-12 15:55:16 +0100

    api changes

--- uat-xnapi/app/routers/recruitments.py
+++ uat-xnapi/app/routers/recruitments.py
@@ -116,8 +116,13 @@ async def recruitment_detail(request: Request, payload: RecruitmentDetailRequest
 
     upstream: Any = None
     try:
+        import json as _json
         async with httpx.AsyncClient(timeout=30.0) as client:
-            response = await client.get(url, json=body, headers=_external_api_headers())
+            response = await client.get(
+                url,
+                content=_json.dumps(body),
+                headers=_external_api_headers(),
+            )
 
         # Always try to parse upstream response
         try:
