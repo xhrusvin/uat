39fc26aa3c877036b5f34d826f5c48e41f7dcb9b
xhrusvin <rusvin@xpresshealth.com>
2026-07-02 12:36:51 +0100

    changes

--- uat-xnapi/app/routers/shifts_db.py
+++ uat-xnapi/app/routers/shifts_db.py
@@ -387,8 +387,24 @@ async def list_shifts_db_post(request: Request, payload: ShiftsDbListRequest):
         s["ghost_booking"]          = 0
         results.append(s)
 
-    return {"success": True, "total": total, "page": payload.page,
-            "per_page": payload.per_page, "data": results}
+    # Aggregate outreach counts
+    automation_count    = await db["outreach"].distinct("shift_id", {"outreach_status": {"$gt": 0}})
+    outreach_active     = await db["outreach"].count_documents({"outreach_status": {"$in": [1, 2, 3]}})
+    outreach_completed  = await db["outreach"].count_documents({"outreach_status": 10})
+    automation_total    = len(set(str(s) for s in automation_count))
+    to_be_filled_count  = total - automation_total
+
+    return {
+        "success":            True,
+        "total":              total,
+        "automation_count":   automation_total,
+        "to_be_filled_count": to_be_filled_count,
+        "outreach_active":    outreach_active,
+        "outreach_completed": outreach_completed,
+        "page":               payload.page,
+        "per_page":           payload.per_page,
+        "data":               results,
+    }
 
 
 
