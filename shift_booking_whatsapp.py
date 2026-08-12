4d958e6bb9a005bfcfdd676c6b26f6df770663e7
xhrusvin <rusvin@xpresshealth.com>
2026-08-12 15:10:57 +0100

    changes

--- shift_booking_whatsapp.py
+++ shift_booking_whatsapp.py
@@ -90,7 +90,7 @@ def register_shift_booking_whatsapp_routes(app):
         if user_id_param:
             query = {"user_id": ObjectId(user_id_param), "call_processed": 0}
         else:
-            query = {"call_processed": 0, "call_enabled": 1}#, "channel": "WhatsApp"
+            query = {"call_processed": 0, "call_enabled": 1, "channel": "WhatsApp"}
 
         record = app.db.shifts_users.find_one(query, sort=[("assigned_at", 1)])
 
