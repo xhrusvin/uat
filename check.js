3d69954876c18ac82b526110d0e5e8f211f94298
xhrusvin <rusvin@xpresshealth.com>
2026-09-17 09:11:34 +0100

    changes

--- check.js
+++
@@ -1,6 +0,0 @@
-// check.js - inspect the exact document for this email
-
-const sourceDb = db.getSiblingDB("xpress_health");
-
-print("=== xpress_health.users doc for sahad2421@gmail.com ===");
-printjson(sourceDb.users.findOne({ email: "sahad2421@gmail.com" }));
